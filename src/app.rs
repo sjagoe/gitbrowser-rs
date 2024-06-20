85f751a5f22d3bc01c645b816a2da75a38610cf5
Simon Jagoe <simon@simonjagoe.com>
2024-06-20 20:25:52 +0300

    Simplify navigation by dispatching on an Enum

--- src/app.rs
+++ src/app.rs
@@ -18,8 +18,9 @@ mod blob_pager;
 mod pagination;
 mod refs_page;
 mod tree_page;
+pub mod navigation;
 
-use crate::app::{blob_pager::BlobPager, refs_page::RefsPage, tree_page::TreePage};
+use crate::app::{blob_pager::BlobPager, refs_page::RefsPage, tree_page::TreePage, navigation::NavigationAction};
 
 enum AppMode {
     ByRef,
@@ -132,7 +133,7 @@ impl<'repo> App<'repo> {
         page.draw(f, area, content_block, reserved_rows);
     }
 
-    pub fn home(&mut self) {
+    pub fn navigate(&mut self, action: NavigationAction) {
         let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
             Box::new(pager)
         } else if let Some(p) = self.tree_pages.last_mut() {
@@ -140,62 +141,18 @@ impl<'repo> App<'repo> {
         } else {
             Box::new(&mut self.refs_page)
         };
-        page.home(self.height);
-    }
 
-    pub fn end(&mut self) {
-        let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
-            Box::new(pager)
-        } else if let Some(p) = self.tree_pages.last_mut() {
-            Box::new(p)
-        } else {
-            Box::new(&mut self.refs_page)
-        };
-        page.end(self.height);
-    }
-
-    pub fn pagedown(&mut self) {
-        let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
-            Box::new(pager)
-        } else if let Some(p) = self.tree_pages.last_mut() {
-            Box::new(p)
-        } else {
-            Box::new(&mut self.refs_page)
-        };
-        page.pagedown(self.height);
-    }
-
-    pub fn pageup(&mut self) {
-        let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
-            Box::new(pager)
-        } else if let Some(p) = self.tree_pages.last_mut() {
-            Box::new(p)
-        } else {
-            Box::new(&mut self.refs_page)
-        };
-        page.pageup(self.height);
-    }
-
-    pub fn next_selection(&mut self) {
-        let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
-            Box::new(pager)
-        } else if let Some(p) = self.tree_pages.last_mut() {
-            Box::new(p)
-        } else {
-            Box::new(&mut self.refs_page)
-        };
-        page.next_selection();
-    }
-
-    pub fn previous_selection(&mut self) {
-        let page: Box<&mut dyn Navigable> = if let Some(pager) = &mut self.blob_pager {
-            Box::new(pager)
-        } else if let Some(p) = self.tree_pages.last_mut() {
-            Box::new(p)
-        } else {
-            Box::new(&mut self.refs_page)
-        };
-        page.previous_selection();
+        match action {
+            NavigationAction::Select => self.select(),
+            NavigationAction::Back => self.back(),
+            NavigationAction::Home => page.home(self.height),
+            NavigationAction::End => page.end(self.height),
+            NavigationAction::PageUp => page.pageup(self.height),
+            NavigationAction::PageDown => page.pagedown(self.height),
+            NavigationAction::NextSelection => page.next_selection(),
+            NavigationAction::PreviousSelection => page.previous_selection(),
+            _ => {},
+        }
     }
 
     pub fn select(&mut self) {
---
+++ src/app/navigation.rs
@@ -0,0 +1,36 @@
+use crossterm::event::{KeyEvent, KeyCode, KeyModifiers};
+
+pub enum NavigationAction {
+    Select,
+    Back,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+    NextSelection,
+    PreviousSelection,
+    Invalid,
+}
+
+impl From<KeyEvent> for NavigationAction {
+    fn from(key: KeyEvent) -> NavigationAction {
+        let b: u8 = KeyModifiers::CONTROL.bits();
+        match (key.code, key.modifiers.bits()) {
+            (KeyCode::Enter, 0) => NavigationAction::Select,
+            (KeyCode::Char('g'), modifiers) => {
+                if modifiers == b {
+                    NavigationAction::Back
+                } else {
+                    NavigationAction::Invalid
+                }
+            },
+            (KeyCode::Home, 0) => NavigationAction::Home,
+            (KeyCode::End, 0) => NavigationAction::End,
+            (KeyCode::PageUp, 0) => NavigationAction::PageUp,
+            (KeyCode::PageDown, 0) => NavigationAction::PageDown,
+            (KeyCode::Up, 0) => NavigationAction::PreviousSelection,
+            (KeyCode::Down, 0) => NavigationAction::NextSelection,
+            _ => NavigationAction::Invalid,
+        }
+    }
+}
