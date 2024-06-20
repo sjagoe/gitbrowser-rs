00ff85750d32be5da3927a506985c3661c112685
Simon Jagoe <simon@simonjagoe.com>
2024-06-20 18:08:11 +0300

    Delete dead code

--- src/main.rs
+++ src/main.rs
@@ -142,55 +142,6 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> Result<bool
                     }
                     _ => {}
                 }
-                // CurrentScreen::Editing if key.kind == KeyEventKind::Press => {
-                //     match key.code {
-                //         KeyCode::Enter => {
-                //             if let Some(editing) = &app.currently_editing {
-                //                 match editing {
-                //                     CurrentlyEditing::Key => {
-                //                         app.currently_editing = Some(CurrentlyEditing::Value);
-                //                     }
-                //                     CurrentlyEditing::Value => {
-                //                         app.save_key_value();
-                //                         app.current_screen = CurrentScreen::Main;
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //         KeyCode::Backspace => {
-                //             if let Some(editing) = &app.currently_editing {
-                //                 match editing {
-                //                     CurrentlyEditing::Key => {
-                //                         app.key_input.pop();
-                //                     }
-                //                     CurrentlyEditing::Value => {
-                //                         app.value_input.pop();
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //         KeyCode::Esc => {
-                //             app.current_screen = CurrentScreen::Main;
-                //             app.currently_editing = None;
-                //         }
-                //         KeyCode::Tab => {
-                //             app.toggle_editing();
-                //         }
-                //         KeyCode::Char(value) => {
-                //             if let Some(editing) = &app.currently_editing {
-                //                 match editing {
-                //                     CurrentlyEditing::Key => {
-                //                         app.key_input.push(value);
-                //                     }
-                //                     CurrentlyEditing::Value => {
-                //                         app.value_input.push(value);
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //         _ => {}
-                //     }
-                // }
             }
             Err(_) => {}
         }
